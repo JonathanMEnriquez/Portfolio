81535782df59f7713820470613b596382ed085ea
user <user@users-MacBook-Pro.local>
2020-08-10 21:59:17 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '.images/logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>HOLA</nav>
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
